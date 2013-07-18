b463d065fb06321ceaf5fb7452d7be44f887c9dd
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-07-18 16:28:28 +0800

    Sync is now done without clearing the whole Storage (#1); Changes happening in other browser tabs will now be sync'd as well

--- ngStorage.js
+++ ngStorage.js
@@ -2,69 +2,82 @@
 
 (function() {
 
-/**
- * @ngdoc overview
- * @name ngStorage
- */
+    /**
+     * @ngdoc overview
+     * @name ngStorage
+     */
 
-angular.module('ngStorage', []).
+    angular.module('ngStorage', []).
 
-/**
- * @ngdoc object
- * @name ngStorage.$localStorage
- * @requires $browser
- * @requires $window
- */
+    /**
+     * @ngdoc object
+     * @name ngStorage.$localStorage
+     * @requires $browser
+     * @requires $window
+     */
 
-factory('$localStorage', _storageFactory('localStorage')).
+    factory('$localStorage', _storageFactory('localStorage')).
 
-/**
- * @ngdoc object
- * @name ngStorage.$sessionStorage
- * @requires $browser
- * @requires $window
- */
+    /**
+     * @ngdoc object
+     * @name ngStorage.$sessionStorage
+     * @requires $browser
+     * @requires $window
+     */
 
-factory('$sessionStorage', _storageFactory('sessionStorage'));
+    factory('$sessionStorage', _storageFactory('sessionStorage'));
 
-function _storageFactory(storageType) {
-    return function(
-        $browser,
-        $window
-    ){
-        var webStorage = $window[storageType],
-            storage = {
-                $clear: function() {
-                    for (var k in storage) {
-                        '$clear' === k || delete storage[k];
+    function _storageFactory(storageType) {
+        return function(
+            $rootScope,
+            $browser,
+            $window
+        ){
+            var webStorage = $window[storageType],
+                $storage = {
+                    $clear: function() {
+                        for (var k in $storage) {
+                            '$clear' === k || delete $storage[k];
+                        }
                     }
-                }
-            },
-            lastStorage;
+                },
+                _last$storage;
 
-        for (var i = 0, k; k = webStorage.key(i); i++) {
-            storage[k] = angular.fromJson(webStorage.getItem(k));
-        }
-
-        lastStorage = angular.copy(storage);
-
-        $browser.addPollFn(function() {
-            if (!angular.equals(storage, lastStorage)) {
-
-                webStorage.clear();
-
-                angular.forEach(storage, function(v, k) {
-                    if (angular.isDefined(v) && '$clear' !== k) {
-                        webStorage.setItem(k, angular.toJson(v));
-                    }
-                });
-
-                lastStorage = angular.copy(storage);
+            for (var i = 0, k; k = webStorage.key(i); i++) {
+                $storage[k] = angular.fromJson(webStorage.getItem(k));
             }
-        });
 
-        return storage;
-    };
-}
+            _last$storage = angular.copy($storage);
+
+            $browser.addPollFn(function() {
+                if (!angular.equals($storage, _last$storage)) {
+
+                    angular.forEach($storage, function(v, k) {
+                        if (angular.isDefined(v) && '$clear' !== k) {
+                            webStorage.setItem(k, angular.toJson(v));
+                        }
+
+                        delete _last$storage[k];
+                    });
+
+                    angular.forEach(_last$storage, function(v, k) {
+                        webStorage.removeItem(k);
+                    });
+
+                    _last$storage = angular.copy($storage);
+                }
+            });
+
+            'localStorage' === storageType && angular.element($window).bind('storage', function(event) {
+                event.newValue ? $storage[event.key] = angular.fromJson(event.newValue) : delete $storage[event.key];
+
+                _last$storage = angular.copy($storage);
+
+                $rootScope.$digest();
+            });
+
+            return $storage;
+        };
+    }
 
 })();
