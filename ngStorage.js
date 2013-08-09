1623451e5a606719a0405ea1211c453914eb28b5
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-08-09 11:43:07 +0800

    Document maintenance

--- ngStorage.js
+++ ngStorage.js
@@ -35,72 +35,74 @@
             '$browser',
             '$window',
 
-        function(
-            $rootScope,
-            $browser,
-            $window
-        ){
-            var webStorage = $window[storageType],
-                $storage = {
-                    $default: function(items) {
-                        for (var k in items) {
-                            angular.isDefined($storage[k]) || ($storage[k] = items[k]);
-                        }
+            function(
+                $rootScope,
+                $browser,
+                $window
+            ){
+                var webStorage = $window[storageType],
+                    $storage = {
+                        $default: function(items) {
+                            for (var k in items) {
+                                angular.isDefined($storage[k]) || ($storage[k] = items[k]);
+                            }
 
-                        return $storage;
+                            return $storage;
+                        },
+                        $reset: function(items) {
+                            for (var k in $storage) {
+                                '$' === k[0] || delete $storage[k];
+                            }
+
+                            return $storage.$default(items);
+                        }
                     },
-                    $reset: function(items) {
-                        for (var k in $storage) {
-                            '$' === k[0] || delete $storage[k];
+                    _last$storage;
+
+                // #8
+                for (var i = 0, k; i < webStorage.length && (k = webStorage.key(i)); i++) {
+                    'ngStorage-' === k.slice(0, 10) && ($storage[k.slice(10)] = angular.fromJson(webStorage.getItem(k)));
+                }
+
+                _last$storage = angular.copy($storage);
+
+                $browser.addPollFn(function() {
+                    if (!angular.equals($storage, _last$storage)) {
+                        angular.forEach($storage, function(v, k) {
+                            if (angular.isDefined(v) && '$' !== k[0]) {
+
+                                // Remove $$hashKey and other things that cannot be stringified
+                                $storage[k] = angular.fromJson(angular.toJson(v));
+
+                                webStorage.setItem('ngStorage-' + k, angular.toJson(v));
+                            }
+
+                            delete _last$storage[k];
+                        });
+
+                        for (var k in _last$storage) {
+                            webStorage.removeItem('ngStorage-' + k);
                         }
 
-                        return $storage.$default(items);
-                    }
-                },
-                _last$storage;
+                        _last$storage = angular.copy($storage);
 
-            for (var i = 0, k; i < webStorage.length && (k = webStorage.key(i)); i++) {
-                'ngStorage-' === k.slice(0, 10) && ($storage[k.slice(10)] = angular.fromJson(webStorage.getItem(k)));
+                        $rootScope.$apply();
+                    }
+                });
+
+                'localStorage' === storageType && angular.element($window).bind('storage', function(event) {
+                    if ('ngStorage-' === event.key.slice(0, 10)) {
+                        event.newValue ? $storage[event.key.slice(10)] = angular.fromJson(event.newValue) : delete $storage[event.key.slice(10)];
+
+                        _last$storage = angular.copy($storage);
+
+                        $rootScope.$apply();
+                    }
+                });
+
+                return $storage;
             }
-
-            _last$storage = angular.copy($storage);
-
-            $browser.addPollFn(function() {
-                if (!angular.equals($storage, _last$storage)) {
-                    angular.forEach($storage, function(v, k) {
-                        if (angular.isDefined(v) && '$' !== k[0]) {
-
-                            // Remove $$hashKey and other things that cannot be stringified
-                            $storage[k] = angular.fromJson(angular.toJson(v));
-
-                            webStorage.setItem('ngStorage-' + k, angular.toJson(v));
-                        }
-
-                        delete _last$storage[k];
-                    });
-
-                    for (var k in _last$storage) {
-                        webStorage.removeItem('ngStorage-' + k);
-                    }
-
-                    _last$storage = angular.copy($storage);
-
-                    $rootScope.$apply();
-                }
-            });
-
-            'localStorage' === storageType && angular.element($window).bind('storage', function(event) {
-                if ('ngStorage-' === event.key.slice(0, 10)) {
-                    event.newValue ? $storage[event.key.slice(10)] = angular.fromJson(event.newValue) : delete $storage[event.key.slice(10)];
-
-                    _last$storage = angular.copy($storage);
-
-                    $rootScope.$apply();
-                }
-            });
-
-            return $storage;
-        }];
+        ];
     }
 
 })();
