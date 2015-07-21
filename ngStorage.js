1f2e3b551c2fbda45bc970f77064654e1b72616e
Tim Rücker <tim.ruecker@ymail.com>
2015-07-21 12:19:35 +0200

    Replacing the no operation functions with angulars intern noop
    It's easier to read and the code compression is higher

--- ngStorage.js
+++ ngStorage.js
@@ -85,7 +85,7 @@
                 }
 
                 // #9: Assign a placeholder object if Web Storage is unavailable to prevent breaking the entire AngularJS app
-                var webStorage = isStorageSupported(storageType) || ($log.warn('This browser does not support Web Storage!'), {setItem: function() {}, getItem: function() {}}),
+                var webStorage = isStorageSupported(storageType) || ($log.warn('This browser does not support Web Storage!'), {setItem: angular.noop, getItem: angular.noop}),
                     $storage = {
                         $default: function(items) {
                             for (var k in items) {
